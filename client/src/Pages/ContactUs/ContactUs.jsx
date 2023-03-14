46ecddb31cc1cdc1f4be0d5e434c2a4b5076062f
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-14 22:53:13 +0100

    add html form

--- client/src/Pages/ContactUs/ContactUs.jsx
+++ client/src/Pages/ContactUs/ContactUs.jsx
@@ -2,7 +2,55 @@ import "./ContactUs.css";
 import { useState, useEffect } from "react";
 
 const ContactUs = () => {
-    return ( "Hello form" );
-}
- 
+  return (
+    <form action="">
+      <div className="inputDiv">
+        <label htmlFor="">First name:</label>
+        <input type="text" name="firstName" />
+      </div>
+
+      <div className="inputDiv">
+        <label htmlFor="">Last name:</label>
+        <input type="text" name="lastName" />
+      </div>
+
+      <div className="inputDiv">
+        <label htmlFor="">Email:</label>
+        <input type="text" name="email" />
+      </div>
+
+      <div className="inputDiv">
+        <label for="phone">Enter your phone number:</label>
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          pattern="[0-9]{2}-[0-9]{2}-[0-9]{3}-[0-9]{3}"
+          required
+        />
+        </div>
+
+        <div className="inputDiv">
+          <label htmlFor="">Your message for us:</label>
+          <input type="text" name="" />
+        </div>
+
+        <div className="inputDiv">
+          <label for="subscribeNews">Subscribe to newsletter?</label>
+          <input
+            type="checkbox"
+            id="subscribeNews"
+            name="subscribe"
+            value="newsletter"
+          />
+        </div>
+
+        <div className="inputDiv">
+          <input type="submit" />
+        </div>
+
+    </form>
+  );
+};
+
 export default ContactUs;
