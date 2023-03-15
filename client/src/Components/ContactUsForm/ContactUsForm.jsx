6bcd08639620fdaab70caff49d2fb7a3536669c5
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-15 13:57:13 +0100

    make post req work

--- client/src/Components/ContactUsForm/ContactUsForm.jsx
+++ client/src/Components/ContactUsForm/ContactUsForm.jsx
@@ -1,21 +1,37 @@
 import "./ContactUsForm.css";
+import React, { useState, useEffect } from "react";
 
-const ContactUsForm = () => {
+const ContactUsForm = ({onSave}) => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  useEffect(() => {
+    console.log(isSubscribed);
+  }, [isSubscribed]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const contact = Object.fromEntries(formData.entries());
+    contact.subscribe = isSubscribed;
+    console.log(contact);
+    onSave(contact)
+  };
+
   return (
-    <form id="contactForm" className="contact-form">
+    <form id="contactForm" className="contact-form" onSubmit={onSubmit}>
       <div className="inputDiv">
         <label htmlFor="firstName">First name:</label>
-        <input type="text" id="firstName" name="firstName" />
+        <input type="text" id="firstName" name="firstName" required />
       </div>
 
       <div className="inputDiv">
         <label htmlFor="lastName">Last name:</label>
-        <input type="text" id="lastName" name="lastName" />
+        <input type="text" id="lastName" name="lastName" required />
       </div>
 
       <div className="inputDiv">
         <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" />
+        <input type="text" id="email" name="email" required />
       </div>
 
       <div className="inputDiv">
@@ -24,14 +40,14 @@ const ContactUsForm = () => {
           type="tel"
           id="phone"
           name="phone"
-          pattern="[0-9]{2}-[0-9]{2}-[0-9]{3}-[0-9]{3}"
+          pattern="[0-9]{2}-[0-9]{2}-[0-9]{7}"
           required
         />
       </div>
 
       <div className="inputDiv">
         <label htmlFor="message">Your message for us:</label>
-        <input type="text" name="message" />
+        <input type="text" name="message" required />
       </div>
 
       <div className="inputDiv">
@@ -40,14 +56,14 @@ const ContactUsForm = () => {
           type="checkbox"
           id="subscribeNews"
           name="subscribe"
-          value="newsletter"
+          defaultChecked={false}
+          onChange={(e) => setIsSubscribed(e.target.checked)}
         />
       </div>
 
       <div className="inputDiv">
         <input type="submit" />
       </div>
-      
     </form>
   );
 };
--- client/src/Pages/ContactUs/ContactUs.jsx
+++ client/src/Pages/ContactUs/ContactUs.jsx
@@ -3,12 +3,27 @@ import { useState, useEffect } from "react";
 import ContactUsForm from "../../Components/ContactUsForm";
 
 const ContactUs = () => {
+
+  const createContact = (contact) => {
+    return fetch("api/contacts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    }).then((res) => res.json());
+  }
+
+  const handlecreateContact = (contact) => {
+    createContact(contact)
+  }
+
   return (
     <>
     <div className="contact-header">
       <h2>How can we help you?</h2>
     </div>
-    <ContactUsForm />
+    <ContactUsForm onSave={handlecreateContact}/>
     </>
   );
 };
