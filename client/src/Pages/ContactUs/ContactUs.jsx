13c775239646d26002ebd57c27b709e98db5fc8d
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-15 14:48:36 +0100

    create ThankYou page

--- client/src/Pages/ContactUs/ContactUs.jsx
+++ client/src/Pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,10 @@
 import "./ContactUs.css";
 import { useState, useEffect } from "react";
 import ContactUsForm from "../../Components/ContactUsForm";
+import ThankYou from "../../Components/ThankYou";
 
 const ContactUs = () => {
+  const [isSubmitted, setIsSubmitted] = useState(true);
 
   const createContact = (contact) => {
     return fetch("api/contacts", {
@@ -12,18 +14,25 @@ const ContactUs = () => {
       },
       body: JSON.stringify(contact),
     }).then((res) => res.json());
-  }
+  };
 
   const handlecreateContact = (contact) => {
-    createContact(contact)
+    createContact(contact).then(() => {
+      setIsSubmitted(true);
+    });
+  };
+
+  const onAnotherMessage = () => {
+    setIsSubmitted(false)
   }
 
   return (
     <>
-    <div className="contact-header">
-      <h2>How can we help you?</h2>
-    </div>
-    <ContactUsForm onSave={handlecreateContact}/>
+      {isSubmitted ? (
+        <ThankYou onButtonClick={onAnotherMessage}/>
+      ) : (
+        <ContactUsForm onSave={handlecreateContact} />
+      )}
     </>
   );
 };
