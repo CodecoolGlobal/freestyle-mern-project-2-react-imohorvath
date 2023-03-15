f5cfa20d744309f9f5fa4c07d0cb256afee87b7e
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-15 11:59:49 +0100

    add contact-us header

--- client/src/Pages/ContactUs/ContactUs.jsx
+++ client/src/Pages/ContactUs/ContactUs.jsx
@@ -4,7 +4,12 @@ import ContactUsForm from "../../Components/ContactUsForm";
 
 const ContactUs = () => {
   return (
+    <>
+    <div className="contact-header">
+      <h2>How can we help you?</h2>
+    </div>
     <ContactUsForm />
+    </>
   );
 };
 
