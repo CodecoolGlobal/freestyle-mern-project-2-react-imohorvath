a5a8d53165a79817bd82579c5786e747040c277e
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-15 21:10:52 +0100

    add cityfilter component

---
+++ client/src/Components/CityFilter/CityFilter.jsx
@@ -0,0 +1,30 @@
+import "./CityFilter.css";
+import React, { useState, useEffect } from 'react';
+
+const CityFilter = ({filterValue, countries, handleFilter}) => {
+
+  return (
+    <div className="city-filter">
+      <div className="city-filter-input">
+        <label htmlFor="country-dropdown">Search for a country...</label>
+        <input
+          className="country-dropdown"
+          type="text"
+          id="country-dropdown"
+          list="countryList"
+          value={filterValue}
+          onChange={(e) => {
+            handleFilter(e);
+          }}
+        />
+        <datalist id="countryList">
+          {countries.map((country, index) => 
+            <option key={index} value={country.name}>{country.name}</option>
+          )}
+        </datalist>
+      </div>
+    </div>
+  );
+};
+
+export default CityFilter;
