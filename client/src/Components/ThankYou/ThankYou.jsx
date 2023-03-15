c167513914ac7abd45654c160cbc492310413da9
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-03-15 22:16:27 +0100

    fix some css

--- client/src/Components/ThankYou/ThankYou.jsx
+++ client/src/Components/ThankYou/ThankYou.jsx
@@ -6,7 +6,7 @@ const ThankYou = ({ onButtonClick }) => {
   return (
     <div className="thankyouDiv">
       <h2>Thank You for your contacting us!</h2>
-      <button onClick={() => onButtonClick()}>Send another message</button>
+      <button id="thankYouButton" onClick={() => onButtonClick()}>Send another message</button>
     </div>
   );
 };
