072dc337a8397ef1cce1ff93ec521f347a373a6d
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-03-17 04:35:53 +0100

    update model and json keys

--- server/model/city.model.js
+++ server/model/city.model.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const CitySchema = new Schema({
-    name: String,
-    country: String,
-    continent: String,
-    timezone: String,
-    population: Number,
-    official_language: String,
-    currency: String,
-    reviews: Number,
-    sights: [String],
-    image_url: String,
-    color: String,
+  name: String,
+  country: String,
+  continent: String,
+  timezone: String,
+  population: Number,
+  officialLanguage: String,
+  currency: String,
+  reviews: Number,
+  sights: [String],
+  imageUrl: String,
+  color: String,
 });
 
 module.exports = mongoose.model("City", CitySchema);
